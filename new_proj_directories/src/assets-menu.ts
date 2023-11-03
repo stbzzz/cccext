27465891c25b2358ee359a33fad591caf3b6ec9f
weschin <kaffee@yeah.net>
2023-11-03 23:25:46 +0800

    create proj base dirs

--- new_proj_directories/src/assets-menu.ts
+++ new_proj_directories/src/assets-menu.ts
@@ -1,20 +1,80 @@
-
 export function onCreateMenu(assetInfo: any) {
     return [
         {
-            label: 'Test',
+            label: '创建项目基础目录',
             click() {
                 /**
                  * bundles/
+                 *      common/
+                 *          remove_me.json
                  *      bundlename1/
                  *      bundlename2/
                  *      bundlename3/
+                 * preload/
+                 *      conf/
+                 *          game.json
                  * scripts/
+                 *      conf/
+                 *          Config.ts
+                 *          CommonDefine.ts
+                 *          Server.ts
+                 *      data/
+                 *          AuthData.ts
+                 *      gui/
+                 *          common/
+                 *              remove_me.json
+                 *          bundlename1/
+                 *          bundlename2/
+                 *          bundlename3/
+                 *          SplashScene.ts
                  *      MyApp.ts
-                 *      PreloadScene.ts
                  */
 
-                console.log('Test');
+                Editor.Message.send('asset-db', 'create-asset', 'db://assets/bundles/common/remove_me.json', `["删除我，我是占位文件"]`);
+                Editor.Message.send('asset-db', 'create-asset', 'db://assets/preload/conf/game.json', `{
+    "mode": "dev",
+    "version": "1.0.0",
+    "servers": {
+        "dev": {
+            "ws": "ws://192.168.3.113:8822/fight",
+            "http": "http://192.168.3.113:8820"
+        },
+        "debug": {},
+        "release": {}
+    }
+}
+                `);
+                Editor.Message.send('asset-db', 'create-asset', 'db://assets/scripts/conf/Config.ts', "export namespace nscfg {}");
+                Editor.Message.send('asset-db', 'create-asset', 'db://assets/scripts/conf/CommonDefine.ts', "export namespace nscommon {}");
+                Editor.Message.send('asset-db', 'create-asset', 'db://assets/scripts/conf/Server.ts', "export namespace nssvr {}");
+                Editor.Message.send('asset-db', 'create-asset', 'db://assets/scripts/data/AuthData.ts', `import { BaseData } from "../../../extensions/frm/assets/src/mgr/DataMgr";
+
+export class AuthData extends BaseData {
+}
+                `);
+                Editor.Message.send('asset-db', 'create-asset', 'db://assets/scripts/gui/common/remove_me.json', `["删除我，我是占位文件"]`);
+                Editor.Message.send('asset-db', 'create-asset', 'db://assets/scripts/gui/SplashScene.ts', `import { BaseScene } from "../../../extensions/frm/assets/src/gui/BaseScene";
+
+import { _decorator } from "cc";
+
+const { ccclass } = _decorator;
+
+@ccclass('SplashScene')
+export class SplashScene extends BaseScene {
+
+}
+                `);
+                Editor.Message.send('asset-db', 'create-asset', 'db://assets/scripts/MyApp.ts', `import { App } from "../../extensions/frm/assets/src/App";
+
+import { _decorator } from "cc";
+
+const { ccclass } = _decorator;
+
+@ccclass('MyApp')
+export class MyApp extends App {
+    protected onCreate(wsUrl?: string, httpUrl?: string) { }
+}
+                `);
             },
         }
     ];
