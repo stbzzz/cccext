81d6e18844950a97f929cff8e2f801763b23e7db
weschin <kaffee@yeah.net>
2023-11-21 10:57:51 +0800

    update trigger

--- frm/assets/src/util/Trigger.ts
+++ frm/assets/src/util/Trigger.ts
@@ -1,5 +1,6 @@
 export class Trigger {
-    public constructor(interval: number) {
+    public constructor(interval: number, autoReset = true) {
+        this._autoReset = autoReset;
         this.reset(interval);
     }
 
@@ -16,12 +17,18 @@ export class Trigger {
         this._interval = interval;
     }
 
+    public isTriggered(): boolean { return this._triggered; }
+    public resetTriggered() { this._triggered = false; }
+
     public trigger(dt: number): boolean {
         if (this._interval < 0) return false;
+
+        if (!this._autoReset && this._triggered) return true;
 
         this._elapsedTime += dt;
 
         if (this._elapsedTime >= this._interval) {
+            this._triggered = true;
             this._elapsedTime -= this._interval;
             return true;
         }
@@ -31,4 +38,6 @@ export class Trigger {
     //private
     private _interval = -1;
     private _elapsedTime = 0;
+    private _autoReset = true;
+    private _triggered = false;
 }
